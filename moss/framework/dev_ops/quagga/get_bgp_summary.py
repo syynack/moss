32ec6246295572ab270ceaa9bb38095cd86226f0
syynack <mlhome96@gmail.com>
2017-07-01 18:21:38 -0700

    Change get_bgp_summary the same way as get_bgp_neighbors to use regex over JSON output from Quagga

--- moss/framework/dev_ops/quagga/get_bgp_summary.py
+++ moss/framework/dev_ops/quagga/get_bgp_summary.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-import json
+import re
 
 def get_bgp_summary(connection):
-    command = 'vtysh -c "show bgp summary json"'
+    command = 'vtysh -c "show bgp summary"'
     output = connection.send_command(command)
 
     if output is None or 'failed' in output:
@@ -12,7 +12,29 @@ def get_bgp_summary(connection):
             'stdout': output
         }
 
-    stdout = json.loads(output)
+    stdout = {"summary": []}
+    info_regexes = [
+        'identifier\s(?P<bgp_rid>([0-9]{1,3}\.){3}[0-9]{1,3}),\slocal\sAS\snumber\s(?P<local_as>[^\s]+)\svrf-id\s(?P<vrf>[^\n]+)',
+        'BGP\stable\sversion\s(?P<table_ver>[^\n]+)',
+        'RIB\sentries\s(?P<rib_entries>[^,]),\susing\s(?P<rib_memory_usage>.*)\sof\smemory',
+        'Peers\s(?P<total_peers>[^,]+),\susing\s(?P<peer_memory_usage>.*)\sof\smemory',
+        'Peer\sgroups\s(?P<total_peer_groups>[^,]),\susing\s(?P<peer_group_memory_usage>.*)\sof\smemory'
+    ]
+    neighbor_regex = '(?P<neighbor_ip>.*)\s\s(?P<version>4)[\s]+(?P<as>[^\s]+)[\s]+(?P<msg_recv>[^\s]+)[\s]+' \
+                     '(?P<msg_sent>[^\s]+)[\s]+(?P<table_ver>[^\s]+)[\s]+(?P<inq>[^\s]+)[\s]+' \
+                     '(?P<outq>[^\s]+)[\s]+(?P<up_down>[^\s]+)[\s]+(?P<state_pfx_recv>[^\s]+)'
+
+    for line in output.splitlines():
+        neighbor_match = re.match(neighbor_regex, line, re.MULTILINE)
+
+        if neighbor_match:
+            stdout['summary'].append(neighbor_match.groupdict())
+        else:
+            for regex in info_regexes:
+                info_match = re.search(regex, line)
+
+                if info_match:
+                    stdout.update(info_match.groupdict())
 
     return {
         'result': 'success',
