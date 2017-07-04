910ad55849ec2814c7cb8951db1f12fb1286e921
syynack <mlhome96@gmail.com>
2017-07-03 19:46:17 -0700

    Add the new decorator changes in __init__.py to only expose decorated functions to moss.framework

--- moss/framework/__init__.py
+++ moss/framework/__init__.py
@@ -1,45 +1,32 @@
 # -*- coding: utf-8 -*-
 
-# Linux operations
-
-# System
-from .linux_commands import linux_get_system_info
-from .linux_commands import linux_get_system_uptime
+# Only expose decorators from moss.framework.
 
 # BGP
-from .quagga_commands import quagga_get_bgp_memory_usage
-from .quagga_commands import quagga_get_bgp_summary
-from .quagga_commands import quagga_get_bgp_neighbors
-from .quagga_commands import quagga_get_bgp_neighbor
-
-# OSPFv3
-from .quagga_commands import quagga_get_ipv6_ospf_interface
-from .quagga_commands import quagga_get_ipv6_ospf_interfaces
-from .quagga_commands import quagga_get_ipv6_ospf_neighbor_brief
-from .quagga_commands import quagga_get_ipv6_ospf_neighbors_brief
-from .quagga_commands import quagga_get_ipv6_ospf_neighbor_detail
-from .quagga_commands import quagga_get_ipv6_ospf_neighbors_detail
-
-# RIB
-from .quagga_commands import quagga_get_ipv6_route_table
-
-# NDP
-from .linux_commands import linux_get_ndp_table
-from .linux_commands import linux_get_ndp_table_stale_entries
-from .linux_commands import linux_get_ndp_table_reachable_entries
-
-# MAC
-from .linux_commands import linux_get_interfaces_mac_address
-
-# LLDP
-from .linux_commands import linux_get_lldp_neighbors
-from .linux_commands import linux_get_lldp_interface
-
-# IPv6
-from .linux_commands import linux_get_ipv6_addresses
+from .decorators import get_bgp_memory_usage, get_bgp_neighbor, get_bgp_neighbors, get_bgp_summary
 
 # Interfaces
-from .linux_commands import linux_get_interfaces_statistics
-from .linux_commands import linux_get_interface_statistics
-from .quagga_commands import quagga_get_interfaces_description
-from .quagga_commands import quagga_get_interface_description
+from .decorators import get_interface_description, get_interfaces_description, get_interface_statistics, \
+                        get_interfaces_statistics
+
+# IPv6
+from .decorators import get_ipv6_addresses
+
+# LLDP
+from .decorators import get_lldp_interface, get_lldp_neighbors
+
+# MAC
+from .decorators import get_interfaces_mac_address
+
+# NDP
+from .decorators import get_ndp_table_reachable_entries, get_ndp_table_stale_entries, get_ndp_table
+
+# OSPFv3
+from .decorators import get_ipv6_ospf_interface, get_ipv6_ospf_interfaces, get_ipv6_ospf_neighbor_brief, \
+                        get_ipv6_ospf_neighbor_detail, get_ipv6_ospf_neighbors_brief, get_ipv6_ospf_neighbors_detail
+
+# RIB
+from .decorators import get_ipv6_route_table
+
+# System
+from .decorators import get_system_info, get_system_uptime
