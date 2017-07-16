c08b959ba8372789f26f8979f2cbcc90847064fc
Matt Lovatt <mlhome96@gmail.com>
2017-07-16 16:59:54 +0100

    Change timers to use current date for start and end

--- moss/core.py
+++ moss/core.py
@@ -2,8 +2,8 @@
 
 import sys
 
-from utils import *
-from modules.decorators import *
+from utils import runtime, timer, module_start_header, module_end_header, end_banner, module_branch_header, colour
+from datetime import datetime
 
 def run_module(connection, module_data):
     '''
@@ -22,7 +22,8 @@ def run_module(connection, module_data):
     dict
     '''
 
-    module_start_time = timer()
+    module_start_timer = timer()
+    module_start_time = str(datetime.now())
     module_start_header(module_data['module'])
 
     target_mod = __import__('modules.' + connection.device_type, globals(), locals(), ['object'], -1)
@@ -34,25 +35,26 @@ def run_module(connection, module_data):
         print str(e)
 
     module_end_header(result['result'])
-    module_end_time = timer()
-    module_run_time = runtime(module_start_time, module_end_time)
+    module_end_timer = timer()
+    module_end_time = str(datetime.now())
+    module_run_time = runtime(module_start_timer, module_end_timer)
 
     module_result = 'success'
-    next_module = module_data['next_module']
+    next_module = module_data.get('next_module')
 
     # Check if we got the result we wanted
     if result['result'] != module_data['success_outcome']:
         module_result = 'fail'
         if module_data['failure_next_module'] == None:
-            end_banner(result['result'])
+            print ''
+            end_banner(module_result)
             print colour(' :: No failure_next_task specified and success_outcome is not failure for {}. Exiting.\n' \
-                .format(module_data['module']), 'red'
+                .format(module_data['module']), 'white'
             )
-
             sys.exit()
 
         next_module = module_data['failure_next_module']
-        task_branch_header(next_module)
+        module_branch_header(next_module)
 
     print ''
 
@@ -90,8 +92,7 @@ def update_module_order(module_order):
         if not module['final']:
             module['next_module'] = module_order[index + 1]['module']
         else:
-            module['next_module'] = 'end'
+            module['next_module'] = ''
 
-    module_order[-1]['next_module'] = 'end'
-
+    module_order[-1]['final'] = True
     return module_order
