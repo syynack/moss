a12d9e1f636ba7f4b332d28833ac244b703583ce
Matt Lovatt <mlhome96@gmail.com>
2017-07-18 16:02:48 +0100

    Add crypt to allow for file encryption, decryption, and editing

---
+++ moss/cli/crypt.py
@@ -0,0 +1,89 @@
+#! /usr/bin/env python
+
+import click
+import sys
+
+from moss.utils import print_data_in_json
+from moss.crypt import MossCrypt
+
+@click.command(short_help = 'Edit a currently encrypted file')
+@click.option('-k', '--key', default=None, help = 'Key to be used for encryption')
+@click.option('-c', '--crypt-file', default=None, help = 'Target encrypted file to be edited')
+def edit(key, crypt_file):
+    if not key:
+        print '-k/--key is a required argument.'
+        sys.exit(1)
+
+    if not crypt_file:
+        print '-c/--crypt-file is a required argument.'
+        sys.exit(1)
+
+    MossCrypt().edit_encrypted_file(key, crypt_file)
+
+
+@click.command(short_help = 'Generate an 256 bit key for encryption')
+def generate_key():
+    print MossCrypt().generate_aes_encryption_key()
+
+
+@click.command(short_help = 'Encrypt a plaintext file with AES-256')
+@click.option('-j', '--json', is_flag=True, help = 'Output in JSON format')
+@click.option('-k', '--key', default=None, help = 'Key to be used for encryption')
+@click.option('-o', '--output-file', default=None, help = 'File for encrypted data to be written to')
+@click.option('-p', '--plaintext-file', default=None, help = 'Target file to be encrypted')
+def lock(json, key, output_file, plaintext_file):
+    if not plaintext_file:
+        print '-p/--plaintext-file is a required argument.'
+        sys.exit(1)
+
+    result = MossCrypt().encrypt_file(key, output_file, plaintext_file)
+
+    if result['result'] == 'fail':
+        print 'Encryption failed: {}'.format(result['reason'])
+        sys.exit(1)
+
+    if not json:
+        print 'Encryption succeeded'
+        print 'Output file: {}'.format(result['data']['output_file'])
+        sys.exit(0)
+
+    print_data_in_json(result)
+
+
+@click.command(short_help = 'Decrypt an encrypted file')
+@click.option('-j', '--json', is_flag=True, help = 'Output in JSON format')
+@click.option('-k', '--key', default=None, help = 'Key to be used for encryption')
+@click.option('-o', '--output-file', default=None, help = 'File for decrypted data to be written to')
+@click.option('-c', '--crypt-file', default=None, help = 'Target file to be decrypted')
+def unlock(json, key, output_file, crypt_file):
+    if not key:
+        print '-k/--key is a required argument.'
+        sys.exit(1)
+
+    if not crypt_file:
+        print '-c/--crypt-file is a required argument.'
+        sys.exit(1)
+
+    result = MossCrypt().decrypt_file(key, output_file, crypt_file)
+
+    if result['result'] == 'fail':
+        print 'Encryption failed: {}'.format(result['reason'])
+        sys.exit(1)
+
+    if not json:
+        print 'Decryption succeeded'
+        print 'Output file: {}'.format(result['data']['output_file'])
+        sys.exit(0)
+
+    print_data_in_json(result)
+
+
+@click.group(short_help = 'Control encryption, decryption, and editing for files')
+def crypt():
+    pass
+
+
+crypt.add_command(edit)
+crypt.add_command(generate_key, name = 'generate-key')
+crypt.add_command(lock)
+crypt.add_command(unlock)
