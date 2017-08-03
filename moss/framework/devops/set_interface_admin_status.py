2901f7c127ac6121297540388497255174fca0bd
Matt Lovatt <mlhome96@gmail.com>
2017-08-03 17:17:35 +0100

    Add first iteration of set_interface_admin_status for Quagga

---
+++ moss/framework/devops/set_interface_admin_status.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python
+
+from moss import register
+
+@register(platform = 'linux')
+def linux_set_interface_admin_status(connection, status = 'down', interface = None):
+    '''
+    Summary:
+    Set an interface adminstrative status in Quagga.
+
+    Arguments:
+    status              string, up/down
+    interface           string, interface name
+
+    Returns
+    dict
+    '''
+
+    commands = {
+        'down': 'vtysh -c "conf t" -c "interface {}" -c "shutdown"',
+        'up': 'vtysh -c "conf t" -c "interface {}" -c "no shutdown"'
+    }
+
+    if not interface:
+        return {
+            'result': 'fail',
+            'reason': 'No interface specified'
+        }
+
+    try:
+        result = connection.send_command(commands[status.lower()])
+    except KeyError as e:
+        return {
+            'result': 'fail',
+            'reason': '{} is not an option'.format(status)
+        }
+
+    if "Can't shutdown interface" in result or "Can't up interface" in result:
+        return {
+            'result': 'fail',
+            'reason': result
+        }
+
+    command = 'vtysh -c "show interface description" | grep {}'.format(interface)
+    verification = connection.send_command(command)
+
+    admin_status = verification.split()[1]
+
+    if admin_status == status.lower():
+        return {
+            'result': 'success',
+            'reason': verification
+        }
+    else:
+        return {
+            'result': 'fail',
+            'reason': verification
+        }
