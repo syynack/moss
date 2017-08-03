e52429e4bbf58767b97f74530d48974a348bbfec
Matt Lovatt <mlhome96@gmail.com>
2017-08-03 17:18:24 +0100

    Fix issue of duplicate logging

--- moss/framework/core/log.py
+++ moss/framework/core/log.py
@@ -2,20 +2,17 @@
 
 import logging
 
-class Logger():
+logger = logging.getLogger('moss')
+logger.setLevel(logging.INFO)
 
-    def __init__(self):
-        logger = logging.getLogger(__name__)
-        logger.setLevel(logging.INFO)
-
-        handler = logging.FileHandler('.moss/module_run.log')
-        handler.setLevel(logging.INFO)
-        formatter = logging.Formatter('%(asctime)s \033[1;32m%(name)s\033[0m \033[1;34m[%(levelname)s]\033[0m %(message)s')
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-
-        self.logger = logger
+try:
+    handler = logging.FileHandler('output/messages.log')
+    formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+except IOError:
+    pass
 
 
-    def log(self, message):
-        self.logger.info(message)
+def log(message):
+    logger.info(message)
