aac199008559d9bb8fe12f452bc585b890759293
Matt Lovatt <mlhome96@gmail.com>
2017-09-01 20:06:27 +0100

    Move build-moss-docker.sh to scipts/

---
+++ scripts/build-moss-docker.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+docker_image_present=$(docker images | grep moss/moss-test-image)
+
+if [[ ! -n "$docker_image_present"  ]]; then
+  echo "Building moss Docker image"
+  sleep 2
+
+  cd ../tests/docker/
+  docker build -t moss/moss-test-image:latest .
+  docker create -it --name moss moss/moss-test-image:latest
+fi
+
+docker_start_result=$(docker start moss)
+
+echo "--------------------- [ Set moss container root password ] ---------------------"
+docker exec -it moss passwd
+
+if [[ $docker_start_result == *"Error response from daemon:"* ]]; then
+  echo "Unable to start moss container."
+fi
+
+docker exec -it moss /etc/moss/run-services.sh
