651be35ce3ad23a46608cd47593489300a4fa611
Matt Lovatt <mlhome96@gmail.com>
2017-09-08 22:44:58 +0100

    Adding docstring

--- moss/framework/devops/get_bgp_summary.py
+++ moss/framework/devops/get_bgp_summary.py
@@ -5,7 +5,67 @@ from moss.framework.decorators import register
 
 @register(platform = 'linux')
 def linux_get_bgp_summary(connection):
-    command = 'vtysh -c "show bgp summary"'
+    '''
+    Summary:
+    Returns a dict containing BGP summary information retrieved from Quagga.
+
+    Example expected output of vtysh -c "show ip bgp summary" is:
+
+    BGP router identifier 1.1.1.1, local AS number 1
+    RIB entries 0, using 0 bytes of memory
+    Peers 16, using 71 KiB of memory
+    Peer groups 1, using 32 bytes of memory
+
+    Neighbor        V    AS MsgRcvd MsgSent   TblVer  InQ OutQ Up/Down  State/PfxRcd
+    fd35:1:1:2::1   4   200       0       0        0    0    0 never    Active
+    fd35:1:1:2::2   4   200       0       0        0    0    0 never    Active
+
+    Total number of neighbors 2
+
+    Returns:
+    dict
+
+    Depends:
+    Quagga
+
+    Example:
+    "stdout": {
+        "peer_group_memory_usage": "32 bytes",
+        "peer_memory_usage": "71 KiB",
+        "rib_entries": "0",
+        "rib_memory_usage": "0 bytes",
+        "summary": [
+            {
+                "as": "200",
+                "inq": "0",
+                "msg_recv": "0",
+                "msg_sent": "0",
+                "neighbor_ip": "fd35:1:1:2::1 ",
+                "outq": "0",
+                "state_pfx_recv": "Active",
+                "table_ver": "0",
+                "up_down": "never",
+                "version": "4"
+            },
+            {
+                "as": "200",
+                "inq": "0",
+                "msg_recv": "0",
+                "msg_sent": "0",
+                "neighbor_ip": "fd35:1:1:2::2 ",
+                "outq": "0",
+                "state_pfx_recv": "Active",
+                "table_ver": "0",
+                "up_down": "never",
+                "version": "4"
+            }
+        ],
+        "total_peer_groups": "1",
+        "total_peers": "2"
+    }
+    '''
+
+    command = 'vtysh -c "show ip bgp summary"'
     output = connection.send_command(command)
 
     if output is None or 'failed' in output:
