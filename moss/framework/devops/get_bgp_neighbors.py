e22d6d3d40fff6c9a97d887eb9d950bde8968de7
Matt Lovatt <mlhome96@gmail.com>
2017-09-11 21:28:40 +0100

    Add support for get bgp neighbors for Juniper

--- moss/framework/devops/get_bgp_neighbors.py
+++ moss/framework/devops/get_bgp_neighbors.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import re
+
 from moss.framework.decorators import register
 
 @register(platform = 'linux')
@@ -53,6 +54,57 @@ def linux_get_bgp_neighbors(connection):
 
                 if match:
                     stdout[neighbor_address].update(match.groupdict())
+
+    return {
+        'result': 'success',
+        'stdout': stdout
+    }
+
+
+@register(platform = 'juniper')
+def juniper_get_bgp_neighbors(connection):
+    command = 'show bgp neighbor'
+    output = connection.send_command(command)
+
+    if output is None or 'error' in output:
+        return {
+            'result': 'fail',
+            'stdout': output
+        }
+
+    stdout = []
+    regex = 'Peer:\s(?P<peer_connection>[^\s]+)\sAS\s(?P<peer_as>[^\s]+).+Local:\s(?P<local_connection>[^\s]+)\sAS\s(?P<local_as>[^\s]+)' \
+            'Type:\s(?P<type>[^\s]+).+State:\s(?P<state>[^\s]+).+Flags:\s<(?P<flags>.*)>' \
+            'Last\sState:\s(?P<last_state>[^\s]+).+Last\sEvent:\s(?P<last_event>.+)' \
+            'Last\sError:\s(?P<last_error>.+)' \
+            'Export:\s\[\s(?P<export>.*)\s\]' \
+            'Options:\s<(?P<options>.*)>' \
+            'Holdtime:\s(?P<holdtime>[^\s]+)\sPreference:\s(?P<preference>[^\s]+)\sLocal\sAS:\s(?P<local_as>[^\s]+)\sLocal\sSystem\sAS:\s(?P<local_sys_as>.+)' \
+            'Number\sof\sflaps:\s(?P<number_of_flaps>.+)' \
+            'Last\sflap\sevent:\s(?P<last_flap_event>.+)' \
+            'Peer\sID:\s(?P<peer_id>[^\s]+).+Local\sID:\s(?P<local_id>[^\s]+).+Active\sHoldtime:\s(?P<active_holdtime>.+)' \
+            'Keepalive\sInterval:\s(?P<keepalive_interval>[^\s]+).+Peer\sindex:\s(?P<peer_index>[^\s]+)' \
+            'BFD:\s(?P<bfd_status>.+)' \
+            'Local\sInterface:\s(?P<local_interface>[^\s]+)' \
+            'NLRI\sfor\srestart\sconfigured\son\speer:\s(?P<configured_peer_nlri>[^\s]+)' \
+            'NLRI\sadvertised\sby\speer:\s(?P<advertised_peer_nlri>[^\s]+)' \
+            'NLRI\sfor\sthis\ssession:\s(?P<session_nlri>[^\s]+)' \
+            'Stale\sroutes\sfrom\speer\sare\skept\sfor:\s(?P<stale_route_lifetime>[^\s]+)' \
+            'RIB\sState:\s(?P<rib_state>.+)' \
+            'Send\sstate:\s(?P<send_state>.+)' \
+            'Active\sprefixes:[\s]+(?P<active_prefixes>[^\s]+)' \
+            'Received\sprefixes:[\s]+(?P<received_prefixes>[^\s]+)' \
+            'Accepted\sprefixes:[\s]+(?P<accepted_prefixes>[^\s]+)' \
+            'Suppressed\sdue\sto\sdamping:[\s]+(?P<dampened_prefixes>[^\s]+)' \
+            'Advertised\sprefixes:[\s]+(?P<advertised_prefixes>[^\s]+)' \
+            'Input\smessages:.+Total\s(?P<input_total>[^\s]+).+Updates\s(?P<input_updates>[^\s]+).+Refreshes\s(?P<input_refreshes>[^\s]+)\sOctets\s(?P<input_octects>[^\s]+)' \
+            'Output\smessages:.+Total\s(?P<input_total>[^\s]+).+Updates\s(?P<input_updates>[^\s]+).+Refreshes\s(?P<input_refreshes>[^\s]+)\sOctets\s(?P<input_octects>[^\s]+)'
+
+    for line in output.splitlines():
+        match = re.match(regex, line, re.MULTILINE)
+
+        if match is not None:
+            stdout.append(match.groupdict())
 
     return {
         'result': 'success',
