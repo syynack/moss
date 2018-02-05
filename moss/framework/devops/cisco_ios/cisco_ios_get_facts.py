2564cf677acdcc7bdb04b59f0aaf9e63abfaf6fd
Matt Lovatt <mlhome96@gmail.com>
2018-02-05 17:00:41 +0000

    Add get_facts devop for cisco_ios, still need to integrate this into the task

--- moss/framework/devops/cisco_ios/cisco_ios_get_facts.py
+++ moss/framework/devops/cisco_ios/cisco_ios_get_facts.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import re
+
 from moss.framework.decorators import register
 
 @register(platform = 'cisco_ios', group = 'devops')
@@ -10,4 +12,61 @@ def cisco_ios_get_facts(connection):
     command = 'show version'
     output = connection.send_command(command)
 
-    return output
+    if output is None or 'failed' in output:
+        return {
+            'result': 'fail',
+            'stdout': output
+        }
+
+    stdout = {}
+    uptime = {"uptime": {}}
+    interfaces = {"interfaces":[]}
+
+    uptime_regexes = [
+        '(?P<minutes>[0-9]{2})\sminutes',
+        '(?P<hours>[0-9]{2})\shours',
+        '(?P<days>[0-9]{2})\sdays',
+        '(?P<years>[0-9]{2})\syears'
+    ]
+
+    interface_regex = '(?P<count>[^\s]+)\s(?P<interface_type>[^\s]+)\sinterfaces'
+
+    regexes = [
+        'Cisco\sIOS\sSoftware,\s(?P<software>[^\s]+)\sSoftware\s\((?P<software_package>[^\)]+)\),\sVersion\s(?P<software_version>[^,]+),\sRELEASE\sSOFTWARE\s\((?P<release_software>.*)\)',
+        'Compiled\s(?P<compiled>.*)\sby',
+        '(?P<model>.*)\s\((?P<cpu>.*)\)\sprocessor.*with\s(?P<bytes_of_memory>.*)\sbytes\sof\smemory.',
+        'Processor\sboard\sID\s(?P<processor_board_id>[^\s]+)',
+        'CPU\sat\s(?P<cpu_clock>[^,]+),',
+        'DRAM\sconfiguration\sis\s(?P<dram_configuration>.*)\swide\swith\sparity\s(?P<parity>[^.]+)',
+        '(?P<nvram>.*)\sbytes\sof\sNVRAM.',
+        '(?P<hostname>.*)\suptime'
+    ]
+
+    for line in output.splitlines():
+        for regex in regexes:
+            data = re.search(regex, line)
+            if data is not None:
+                data = data.groupdict()
+                stdout.update(data)
+        for uptime_regex in uptime_regexes:
+            uptime_data = re.search(uptime_regex, line)
+            if uptime_data is not None:
+                uptime_data = uptime_data.groupdict()
+                uptime["uptime"].update(uptime_data)
+        for int_regex in interface_regex:
+            int_data = re.search(interface_regex, line)
+            if int_data is not None:
+                int_data = int_data.groupdict()
+                try:
+                    if int_data["interface_type"] != interfaces["interfaces"][-1].get("interface_type"):
+                        interfaces["interfaces"].append(int_data)
+                except IndexError:
+                    interfaces["interfaces"].append(int_data)
+
+    stdout.update(uptime)
+    stdout.update(interfaces)
+
+    return {
+        "result": "success",
+        "stdout": stdout
+    }
